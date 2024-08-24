86b1969da4cd277d2197b155aec1ba4d2a9458d4
taarun-r <taarun.r@gmail.com>
2024-08-24 14:46:35 +0530

    Personal learing

---
+++ Day 1/String_Manupliation.py
@@ -0,0 +1,14 @@
+# input("what is your name ?")
+# print("hello " + input("what is your name ?")+ "!")
+
+# Variables in python 
+
+# name = input("what is your name ?")
+# print(name)
+
+# name = input("What is your name ?")
+# print(len(input("what is your name ?")))   
+
+username = input("what is your username ?")
+length = len (username)
+print(length)
---
+++ Day 2/notes.py
@@ -0,0 +1,31 @@
+# Primitive Data Types
+
+# String -> Ex: "Hello" print("Hello"[0]) - pulling a postion of a character from string is called (Subscripting)
+# print("Hello"[4])
+
+# Integer = Whole Number
+# print(123+456)
+
+# Large Integers
+# print(1234_567_890) intead of commas, for large number underscore is used 123,456 -> 123_456 in python
+
+# Float - numbers with decimal point 
+#  eg : 1.5
+
+# Boolean
+# True or False 
+
+# To check the data-type
+print(type("Hello"))
+print(type(1234))
+print(type(3.14))
+print(type(True))
+
+# Type Converation AKA Type Casting 
+
+# print(int(123)+int(465)) -> Example of type conversation
+
+# int()
+# float()
+# str()
+# bool()
